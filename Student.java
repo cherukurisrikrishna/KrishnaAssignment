8b35ce725083c7708a1ec7df52f830d9ecfc569c
cherukurisrikrishna <cherukurisrikrishna931@gmail.com>
2024-01-10 21:05:19 -0600

    First Push

---
+++ Student.java
@@ -0,0 +1,16 @@
+package Ass1;
+
+public class Student
+{
+ String name="Krishna";
+ String getName()
+ {
+	 return name;
+ }
+ public static void main(String[] args)
+ {
+	 Student s1=new Student();
+	 System.out.println(s1.getName());
+	
+}
+}
