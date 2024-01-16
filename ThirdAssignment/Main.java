b31937f7acf44466579f8d7cd06e78783e0d7d02
cherukurisrikrishna <cherukurisrikrishna931@gmail.com>
2024-01-16 09:32:34 -0600

    3rd assignment

---
+++ ThirdAssignment/Main.java
@@ -0,0 +1,15 @@
+package ThirdAssignment;
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println(StaticCode.i);
+		
+		StaticCode.i=25;
+		System.out.println(StaticCode.m1());
+		
+	}
+	
+	
+
+}
---
+++ ThirdAssignment/StaticCode.java
@@ -0,0 +1,12 @@
+package ThirdAssignment;
+
+public class StaticCode 
+{
+	public static int i=10;
+	public static int m1()
+	{
+		return i;
+	}
+	
+
+}
