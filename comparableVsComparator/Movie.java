65b90105b8475830be5ecfbdb9b18fb9ec16f04e
cherukurisrikrishna <cherukurisrikrishna931@gmail.com>
2024-01-19 10:00:39 -0600

    compare

---
+++ comparableVsComparator/Movie.java
@@ -0,0 +1,71 @@
+package comparableVsComparator;
+
+//Create a class named Movie with variables like yearReleased, rating, budget, coleectionAmount. 
+//Write two Comparator implementations to sort the Movie by
+//
+//rating and profit
+//year released and rating
+
+public class Movie 
+{
+	
+	private int yearReleased;
+	private float rating;
+	private int budget;
+	private int collectionAmount;
+	
+	private int profit = collectionAmount-budget;
+
+	public int getYearReleased() {
+		return yearReleased;
+	}
+
+	public void setYearReleased(int yearReleased) {
+		this.yearReleased = yearReleased;
+	}
+
+	public float getRating() {
+		return rating;
+	}
+
+	public void setRating(float rating) {
+		this.rating = rating;
+	}
+
+	public long getBudget() {
+		return budget;
+	}
+
+	public void setBudget(int budget) {
+		this.budget = budget;
+	}
+
+	public long getCollectionAmount() {
+		return collectionAmount;
+	}
+
+	public void setCollectionAmount(int collectionAmount) {
+		this.collectionAmount = collectionAmount;
+	}
+
+	public int getProfit() {
+		return profit;
+	}
+
+	public void setProfit(int profit) {
+		this.profit = profit;
+	}
+
+	public Movie(int yearReleased, float rating, int budget, int collectionAmount) {
+		
+		this.yearReleased = yearReleased;
+		this.rating = rating;
+		this.budget = budget;
+		this.collectionAmount = collectionAmount;
+		
+	}
+	
+	
+	
+
+}
